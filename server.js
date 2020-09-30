7003e0d8f51cb63d41d3cf6a1fd320a39dc76a01
Ashish Kumar <ashish.kmr@outlook.com>
2020-10-01 02:44:03 +0530

    add host

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ fastify.register(require('./routes/definations'))
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 8000);
+    await fastify.listen(process.env.PORT, '0.0.0.0' || 8000);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
